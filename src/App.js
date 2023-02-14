ccdcf4fba0fcc3dfbc100b38fe8f44cdc9027e4b
at77777 <vxjhxz5cfuqn6jp7xwrslxtk@gmail.com>
2023-02-14 16:50:36 +0200

    add lesson2-space-x-react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
-import flag from './flag.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [launches, setLaunches] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.spacexdata.com/v3/launches/`).then(resp => resp.json()).then(resp => setLaunches(resp))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Glory to Ukraine!
-        </p>
-        <img src={flag} className="App-flag" alt="flag" />
-        <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </p>
-      </header>
+      {launches.filter(item => item.launch_year !== '2020').map(item => {
+        return (
+            <div key={item.flight_number}>
+              {item.flight_number} --- {item.mission_name} --- {item.launch_year}
+            </div>
+        )
+      })}
     </div>
   );
 }
